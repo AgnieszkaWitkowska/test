0137f5f22fb937435f614eaeef465964446483e8
AgnieszkaWitkowska <agnieszkakojro@wp.pl>
2018-10-26 10:49:26 +0200

    nowe ooo

--- main.c
+++ main.c
@@ -3,25 +3,14 @@
 
         int main(){
 
-    int a;
+   float suma=1573.821, skladnik = 3.51;
+   int licznik=0;
 
-printf("Podaj liczbę mniejszą niż 20, a wiekszą niż 0: \n");
-scanf("%d", &a);
-
-if (!(a%2)) --a;
-for(; a>0; a-=2)
-{
-    if ((!a%2)) continue;
-        if (!(a%3)) continue;
-        if (!(a%5)) continue;
-        if (!(a%7)) continue;
-
-printf("%d ", a);}
-
-
-
-
-    return 0;
-
-
-}
+   while (suma>1E-10) {
+       suma -= skladnik;
+       skladnik *= skladnik;
+       ++licznik;
+   }
+   printf("%d", licznik);
+   return 0;
+   }
