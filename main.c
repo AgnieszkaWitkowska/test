7368ef6677422b6559baeee1f2baa907d718bd4f
AgnieszkaWitkowska <agnieszkakojro@wp.pl>
2018-10-26 08:34:11 +0200

    ggg

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+        int main(){
+
+    int a;
+    int licznik_petli;
+
+            for ( a = 100; a >= 1; a-=2) {
+                {
+            printf("Liczby naturalne od 100 do 1 to: %d\n",a);}
+
+
+           licznik_petli++; }
+           printf("\n");
+            printf("liczba pętli wynosi t: %d\n", licznik_petli);
+
+    return 0;
+
+
+}
